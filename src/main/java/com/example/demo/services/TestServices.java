dd126d58f50409545eec97e219199ecab5a5ba83
Dantesh Kumar <danteshkumar@Danteshs-MacBook-Air.local>
2020-05-09 20:10:13 +0530

    Practice code

---
+++ src/main/java/com/example/demo/services/TestServices.java
@@ -0,0 +1,12 @@
+package com.example.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestServices {
+
+	public String test()
+	{
+		return "Hello World !!!";
+	}
+}
